813d0bd7ffc349a7b9f02eb35791f5914fae1a23
gabrielmmoraes <gabrielm.moraes@hotmail.com>
2018-04-07 22:17:01 -0300

    Basics and Printing 6

---
+++ basics-and-printing/6/LetterToYourself.java
@@ -0,0 +1,19 @@
+public class LetterToYourself{
+    public static void main (String args[]){
+        System.out.println("+------------------------------------------------------------------------------------+");
+        System.out.println("|                                                                              ##### |");
+        System.out.println("|                                                                              ##### |");
+        System.out.println("|                                                                              ##### |");
+        System.out.println("|                                                                                    |");
+        System.out.println("|                                                                                    |");
+        System.out.println("|                                                                                    |");
+        System.out.println("|                                                                                    |");
+        System.out.println("|                                                                                    |");
+        System.out.println("|                                                           Gabriel Mendes           |");
+        System.out.println("|                                                           Random Phrase            |");
+        System.out.println("|                                                           Random Avenue            |");
+        System.out.println("|                                                                                    |");
+        System.out.println("|                                                                                    |");
+        System.out.println("+------------------------------------------------------------------------------------+");
+    }
+}
